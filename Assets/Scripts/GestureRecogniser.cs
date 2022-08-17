93fbd1e30706f62a90a9729311931e9223b0c7db
Samuel liu <86864666+CynicalRadicand@users.noreply.github.com>
2022-08-17 13:42:30 +1200

    Added L Gesture

--- Assets/Scripts/GestureRecogniser.cs
+++ Assets/Scripts/GestureRecogniser.cs
@@ -11,27 +11,21 @@ public class GestureRecogniser : MonoBehaviour
 {
     private bool isDoneExecuting = true;
 
-    /// <summary>
-    /// Threshold for defining a curled finger
-    /// </summary>
-    public float angleCurlThreshold = 0.55f;
+    public float LCurlThreshold = 0.5f;
+    public float LIndexThreshold = 0.1f;
+    public float LThumbThreshold = 0.2f;
 
-    /// <summary>
-    /// Threshold for defining a straight finger
-    /// </summary>
-    public float angleStraightThreshold = 0.25f;
-    public float thumbsUpCurlThreshold = 0.65f;
-    public float thumbsUpStraightThreshold = 0.25f;
+    public float thumbsUpCurlThreshold = 0.6f;
+    public float thumbsUpStraightThreshold = 0.3f;
+
     public float pointCurlThreshold = 0.5f;
     public float pointStraightThreshold = 0.2f;
-    public float facingCameraTrackingThreshold = 60.0f;
-    public float facingAwayFromCameraTrackingThreshold = 120.0f;
-    public float flatHandThreshold = 45.0f;
+
     public float pinchIndexThreshold = .25f;
     public float pinchThumbThreshold = .45f;
     public float doublePinchDistance = .1f;
 
-    public TextMeshPro textMeshProHit;
+    public TextMeshPro currentGestureText;
     public TextMeshPro angleText;
     public TextMeshPro fingerPosText;
 
@@ -65,33 +59,33 @@ public class GestureRecogniser : MonoBehaviour
     {
         if (checkThumbs("Up"))
         {
-            textMeshProHit.SetText("Thumbs Up");
+            currentGestureText.SetText("Thumbs Up");
         }
         else if (checkThumbs("Down"))
         {
-            textMeshProHit.SetText("Thumbs Down");
+            currentGestureText.SetText("Thumbs Down");
         }
         else if (isPointDown(rightHand))
         {
-            textMeshProHit.SetText("Pointing Down Right");
+            currentGestureText.SetText("Pointing Down Right");
             anchor.Summon(getFingerPos(rightHand));
         }
         else if (isPointDown(leftHand))
         {
-            textMeshProHit.SetText("Pointing Down Left");
+            currentGestureText.SetText("Pointing Down Left");
             anchor.Summon(getFingerPos(leftHand));
         }
         else if (isIndexPointed(rightHand) || isIndexPointed(leftHand))
         {
-            textMeshProHit.SetText("Pointing");
+            currentGestureText.SetText("Pointing");
         }
         else if (isDoublePinch())
         {
-            textMeshProHit.SetText("Double Pinch");
+            currentGestureText.SetText("Double Pinch");
         }
         else
         {
-            textMeshProHit.SetText("NONE");
+            currentGestureText.SetText("NONE");
         }
     }
     protected void fingerAngle()
@@ -127,36 +121,20 @@ public class GestureRecogniser : MonoBehaviour
 
         HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, hand, out MixedRealityPose indexTipPose);
 
-        
+
         string s = string.Format("Right Index Position = {0}", indexTipPose.Position);
 
         fingerPosText.SetText(s);
 
         return indexTipPose.Position;
     }
-    protected bool checkAngle(Handedness hand)
-    {
-        if (checkL(hand) && checkPalmFacingConstraint(hand, false, false))
-        {
-            if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, hand, out MixedRealityPose indexTipPose) && HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexKnuckle, hand, out MixedRealityPose indexKnucklePose))
-            {
-                Vector3 indexDirection = indexTipPose.Position - indexKnucklePose.Position;
-                float indexCameraAngle = Vector3.Angle(indexDirection, CameraCache.Main.transform.up);
-
-                return indexCameraAngle < 60;
-
-            }
-        }
-        return false;
-
-    }
     protected bool checkL(Handedness hand)
     {
-        if (HandPoseUtils.ThumbFingerCurl(hand) <= angleStraightThreshold &&
-            HandPoseUtils.IndexFingerCurl(hand) <= angleStraightThreshold &&
-            HandPoseUtils.MiddleFingerCurl(hand) > angleCurlThreshold &&
-            HandPoseUtils.RingFingerCurl(hand) > angleCurlThreshold &&
-            HandPoseUtils.PinkyFingerCurl(hand) > angleCurlThreshold)
+        if (HandPoseUtils.ThumbFingerCurl(hand) <= LThumbThreshold &&
+            HandPoseUtils.IndexFingerCurl(hand) <= LIndexThreshold &&
+            HandPoseUtils.MiddleFingerCurl(hand) >= LCurlThreshold &&
+            HandPoseUtils.RingFingerCurl(hand) >= LCurlThreshold &&
+            HandPoseUtils.PinkyFingerCurl(hand) >= LCurlThreshold)
         {
             return true;
         }
@@ -207,31 +185,10 @@ public class GestureRecogniser : MonoBehaviour
         }
         return false;
     }
-    protected bool checkDistance()
-    {
-        if (isIndexPointed(rightHand) && isIndexPointed(leftHand))
-        {
-            if (isFacingTowardsCentre(rightHand) && isFacingTowardsCentre(leftHand))
-            {
-                // Check they are relatively same height
-                if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, rightHand, out MixedRealityPose rightIndexTipPose) && HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, leftHand, out MixedRealityPose leftIndexTipPose))
-                {
-                    bool isSameHeight = Mathf.Abs(rightIndexTipPose.Position.y - leftIndexTipPose.Position.y) < 0.05;
-                    // Check right is on the right, and left is on the left
-                    bool isCorrectPosition = (rightIndexTipPose.Position.x - leftIndexTipPose.Position.x) > 0;
-
-                    return isSameHeight && isCorrectPosition;
-                }
-
-            }
-        }
-
-        return false;
-    }
 
     private bool isIndexPointed(Handedness hand)
     {
-        if (HandPoseUtils.ThumbFingerCurl(hand) > pointCurlThreshold &&
+        if (//HandPoseUtils.ThumbFingerCurl(hand) > pointCurlThreshold &&
             HandPoseUtils.IndexFingerCurl(hand) <= pointStraightThreshold &&
             HandPoseUtils.MiddleFingerCurl(hand) > pointCurlThreshold &&
             HandPoseUtils.RingFingerCurl(hand) > pointCurlThreshold &&
@@ -272,58 +229,6 @@ public class GestureRecogniser : MonoBehaviour
             return true;
         }
 
-        return false;
-    }
-
-
-    private bool isFacingTowardsCentre(Handedness hand)
-    {
-        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexKnuckle, hand, out MixedRealityPose indexKnucklePose) &&
-            HandJointUtils.TryGetJointPose(TrackedHandJoint.RingKnuckle, hand, out MixedRealityPose ringKnucklePose) &&
-            HandJointUtils.TryGetJointPose(TrackedHandJoint.Wrist, hand, out MixedRealityPose wristPose))
-        {
-            var handNormal = Vector3.Cross(indexKnucklePose.Position - wristPose.Position,
-                                                  ringKnucklePose.Position - wristPose.Position).normalized;
-
-            return Vector3.Angle(CameraCache.Main.transform.right, handNormal) < 30;
-        }
-        return false;
-    }
-
-    private bool checkPalmFacingConstraint(Handedness hand, bool inwards, bool flatHand)
-    {
-        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, hand, out MixedRealityPose palmPose))
-        {
-            float palmCameraAngle = Vector3.Angle(palmPose.Up, CameraCache.Main.transform.forward);
-
-            if (flatHand)
-            {
-                // Check if the triangle's normal formed from the palm, to index, to ring finger tip roughly matches the palm normal.
-                MixedRealityPose indexTipPose, ringTipPose;
-
-                if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, hand, out indexTipPose) &&
-                    HandJointUtils.TryGetJointPose(TrackedHandJoint.RingTip, hand, out ringTipPose))
-                {
-                    var handNormal = Vector3.Cross(indexTipPose.Position - palmPose.Position,
-                                                   ringTipPose.Position - indexTipPose.Position).normalized;
-                    handNormal *= (hand == Handedness.Right) ? 1.0f : -1.0f;
-
-                    if (Vector3.Angle(palmPose.Up, handNormal) > flatHandThreshold)
-                    {
-                        return false;
-                    }
-                }
-            }
-            // Check if the palm angle meets the prescribed threshold for the direction
-            if (inwards)
-            {
-                return palmCameraAngle < facingCameraTrackingThreshold;
-            }
-            else
-            {
-                return palmCameraAngle > facingAwayFromCameraTrackingThreshold;
-            }
-        }
         return false;
     }
 }
