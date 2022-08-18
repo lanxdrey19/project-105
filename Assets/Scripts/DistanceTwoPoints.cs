cf8d09ba9aa1e46d96d1e47d9379f0b186ddaba4
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-08-19 06:13:06 +1200

    add measure distance/area

---
+++ Assets/Scripts/DistanceTwoPoints.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DistanceTwoPoints : MonoBehaviour
+{
+    public GameObject Point1;
+    public GameObject Point2;
+    public float DistanceBetweenPoints;
+    public TMP_Text DistanceText;
+    public LineRenderer LineBetweenPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceBetweenPoints = Vector3.Distance(Point1.transform.position, Point2.transform.position);
+        DistanceText.text = "Distance: " + DistanceBetweenPoints.ToString();
+        LineBetweenPoints.SetPosition(0, Point1.transform.position);
+        LineBetweenPoints.SetPosition(1, Point2.transform.position);
+    }
+}
