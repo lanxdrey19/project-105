2eea2322584291e1c0b6b407edb467750d5d4a0e
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-08-27 01:16:11 +1200

    spawn nodes relative to camera direction and minor aesthetic changes

--- Assets/Scripts/PositionNearPerson.cs
+++ Assets/Scripts/PositionNearPerson.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities;
 
 public class PositionNearPerson : MonoBehaviour
 {
+    public void PositionInFront(GameObject gameObject)
+    {
+        Vector3 position = CameraCache.Main.transform.position + CameraCache.Main.transform.forward * 0.2f;
+        position = new Vector3(position.x, 0, position.z);
+        gameObject.transform.position = position;
+    
+    }
     public void PositionSlightLeft(GameObject gameObject)
     {
-        
-        Vector3 position = Camera.main.transform.position;
+        Vector3 position = CameraCache.Main.transform.position + CameraCache.Main.transform.forward * 0.2f;
+        position = new Vector3(position.x, 0, position.z);
         gameObject.transform.position = position;
         gameObject.transform.Translate(-0.1f, 0f, 0f);
     }
@@ -15,7 +23,8 @@ public class PositionNearPerson : MonoBehaviour
     public void PositionSlightRight(GameObject gameObject)
     {
 
-        Vector3 position = Camera.main.transform.position;
+        Vector3 position = CameraCache.Main.transform.position + CameraCache.Main.transform.forward * 0.2f;
+        position = new Vector3(position.x, 0, position.z);
         gameObject.transform.position = position;
         gameObject.transform.Translate(0.1f, 0f, 0f);
     }
@@ -23,17 +32,20 @@ public class PositionNearPerson : MonoBehaviour
     public void PositionSlightLeftFar(GameObject gameObject)
     {
 
-        Vector3 position = Camera.main.transform.position;
+        Vector3 position = CameraCache.Main.transform.position + CameraCache.Main.transform.forward * 0.2f;
+        position = new Vector3(position.x, 0, position.z);
         gameObject.transform.position = position;
-        gameObject.transform.Translate(-0.15f, 0.1f, 0.1f);
+        gameObject.transform.Translate(-0.1f, -0.1f, 0.1f);
+
     }
 
     public void PositionSlightRightFar(GameObject gameObject)
     {
-
-        Vector3 position = Camera.main.transform.position;
+        Vector3 position = CameraCache.Main.transform.position + CameraCache.Main.transform.forward * 0.2f;
+        position = new Vector3(position.x, 0, position.z);
         gameObject.transform.position = position;
-        gameObject.transform.Translate(0.15f, 0.1f, 0.1f);
+        gameObject.transform.Translate(0.1f, -0.1f, 0.1f);
+
     }
 
     public void PositionFireInVirtualButtonScene(GameObject gameObject)
